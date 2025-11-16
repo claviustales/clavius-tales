0faeaa1ab8d98d83dc0e5b16dbef4106020b2acb
Clavius Tales <clavius.tales@gmail.com>
2025-11-16 17:53:17 -0300

    Refine RuboCop config and split request specs

---
+++ spec/clavius_tales/app_spec.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe ClaviusTales::App do
+  describe 'GET /' do
+    subject(:response) do
+      get '/'
+      last_response
+    end
+
+    let(:response_body) { JSON.parse(response.body) }
+
+    it 'responds with 200' do
+      expect(response).to be_ok
+    end
+
+    it 'returns JSON content type' do
+      expect(response.headers['Content-Type']).to eq('application/json')
+    end
+
+    it 'returns welcome message' do
+      expect(response_body['message']).to eq('Bem-vindo ao backend da Clavius Tales')
+    end
+  end
+
+  describe 'GET /health' do
+    subject(:response) do
+      get '/health'
+      last_response
+    end
+
+    let(:response_body) { JSON.parse(response.body) }
+
+    it 'responds with 200' do
+      expect(response).to be_ok
+    end
+
+    it 'returns ok status' do
+      expect(response_body['status']).to eq('ok')
+    end
+
+    it 'returns the current time' do
+      expect(response_body['time']).not_to be_nil
+    end
+  end
+
+  describe 'GET /unknown' do
+    subject(:response) do
+      get '/unknown'
+      last_response
+    end
+
+    let(:response_body) { JSON.parse(response.body) }
+
+    it 'responds with 404' do
+      expect(response.status).to eq(404)
+    end
+
+    it 'returns not found message' do
+      expect(response_body['error']).to eq('Rota não encontrada')
+    end
+  end
+end
